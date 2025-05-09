bcb113b8708da1433a5687544adfa92cc76b2ff0
ApocalypseAlbino <110875387+ApocalypseAlbino@users.noreply.github.com>
2025-05-09 14:38:51 +1200

    Created the base GUI and buttons for the quiz

--- C_02_Quiz_Component.py
+++ C_02_Quiz_Component.py
@@ -19,7 +19,6 @@ class StartQuiz:
         intro_string = "This is a multi-choice quiz about quotes from movies. You will either select which movie a " \
                        "quote comes from, or select which quote belongs to a certain movie."
 
-        # choose_string = "Oops - Please choose a whole number more than 0."
         choose_string = "How many questions do you want?"
 
         # List of labels to be made (text | font)
@@ -65,20 +64,24 @@ class StartQuiz:
         # Retrieve amount of rounds wanted
         rounds_wanted = self.num_rounds_entry.get()
 
-        # Reset label and entry box (for when users come back to home screen
-        self.choose_label.config(fg="#009900", font=("Arial", "12", "bold"))
+        # Reset label and entry box
+        self.choose_label.config(fg="#000", font=("Arial", "12", "bold"),
+                                 text="How many questions do you want?")
         self.num_rounds_entry.config(bg="#fff")
+        self.num_rounds_entry.delete(0, END)
 
         error = "Please choose a whole number more than 0"
         has_errors = "no"
 
-        # checks that amount to be converted is a number above absolute 0
+        # checks that amount to be converted is a number above 0
         try:
             rounds_wanted = int(rounds_wanted)
             if rounds_wanted > 0:
-                # temporary success message, replace with call to PlayGame class
-                self.choose_label.config(text=f"You have chosen to play {rounds_wanted} "
-                                              f"round/s")
+                # Invoke Play Class (and take across number of rounds)
+                Play(rounds_wanted)
+                # Hide root window (ieL hide rounds choice window)
+                root.withdraw()
+
             else:
                 has_errors = "yes"
 
@@ -90,6 +93,82 @@ class StartQuiz:
             self.choose_label.config(text=error, fg="#990000")
             self.num_rounds_entry.config(bg="#f4cccc")
             self.num_rounds_entry.delete(0, END)
+
+
+class Play:
+    """
+    Interface for the quiz
+    """
+
+    def __init__(self, how_many):
+        self.play_box = Toplevel()
+
+        self.game_frame = Frame(self.play_box)
+        self.game_frame.grid(padx=10, pady=10)
+
+        self.game_heading_label = Label(self.game_frame, text="Question",
+                                        font=("Arial", "16", "bold"))
+        self.game_heading_label.grid(row=0, pady=10)
+
+        # Colour buttons
+        self.colour_frame = Frame(self.game_frame)
+        self.colour_frame.grid(row=3)
+
+        # colour buttons (text | bg colour | command | row | column)
+        answer_details_list = [
+            ["Answer", "#f0f0f0", "", 0, 0],
+            ["Answer", "#f0f0f0", "", 0, 1],
+            ["Answer", "#f0f0f0", "", 1, 0],
+            ["Answer", "#f0f0f0", "", 1, 1]
+        ]
+
+        # List to hold buttons once they have been made
+        self.answer_ref_list = []
+
+        for item in answer_details_list:
+            self.make_button = Button(self.colour_frame,
+                                      text=item[0], bg=item[1],
+                                      fg="#000", font=("Arial", "12"),
+                                      width=22, command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+
+            self.answer_ref_list.append(self.make_button)
+
+        self.result_label = Label(self.game_frame, text="Correct/Incorrect! Good/Bad job!",
+                                  font=("Arial", "12"), bg="#b4daa9")
+        self.result_label.grid(row=4, pady=10)
+
+        # Other buttons
+        self.button_frame = Frame(self.game_frame)
+        self.button_frame.grid(row=6)
+
+        # buttons (text | bg colour | command | row | column)
+        button_details_list = [
+            [f"Next Round (1/{how_many})", "#f0f0f0", "", 0, 1],
+            ["Instructions", "#f0f0f0", "", 0, 0]
+        ]
+
+        # List to hold buttons once they have been made
+        self.button_ref_list = []
+
+        for item in button_details_list:
+            self.make_button = Button(self.button_frame,
+                                      text=item[0], bg=item[1],
+                                      fg="#000", font=("Arial", "16", "bold"),
+                                      width=15, command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=6)
+
+        self.end_game_button = Button(self.game_frame, text="End Game",
+                                      font=("Arial", "16", "bold"),
+                                      fg="#000", bg="#f0f0f0",
+                                      command=self.close_play,
+                                      width=32)
+        self.end_game_button.grid(row=7, pady=10)
+
+    def close_play(self):
+        # Reshow root and end current quiz
+        root.deiconify()
+        self.play_box.destroy()
 
 
 # main routine
